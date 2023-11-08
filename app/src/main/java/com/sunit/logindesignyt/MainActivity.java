04ffcdd6da68ff4210d5beeff5ee47cf78fc36fe
DangTranTuanAnh <dangtrantuananh.2002@gmail.com>
2023-11-08 11:01:57 +0700

    first commit

---
+++ app/src/main/java/com/sunit/logindesignyt/MainActivity.java
@@ -0,0 +1,17 @@
+package com.sunit.logindesignyt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sunit.logindesignyt.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
